6e72358d9504d433a6f2055c1ef1b16f61df2c7d
sonia13marker <soniamarker13@gmail.com>
2023-01-21 23:57:44 +0300

    изменение главной

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -7,18 +7,22 @@ export function MainPage () {
     return <div>
         <HeaderMenuFull />
         <main className={style.main}>
+           <div className={style.leftBlock}>
             <div className={style.main__alltext}>
-                <h1 className={style.main__h1}>Creative.CT - </h1>
-                <h2 className={style.main__h2}>твое творческое</h2>
-                <h2 className={style.main__h2}>состязание</h2>
-                <button className={style.main__btn}>Подать заявку <span className={style.btn__arr}></span><img src={arrow} alt='arrow'/></button>
-            </div>
-            <div className={style.main__block}></div>
-            <hr className={style.main__line}></hr>
+                    <h1 className={style.main__h1}>Creative.CT - </h1>
+                    <h2 className={style.main__h2}>твое творческое</h2>
+                    <h2 className={style.main__h2}>состязание</h2>
+                    <button className={style.main__btn}>Подать заявку <span className={style.btn__arr}></span><img src={arrow} alt='arrow'/></button>
+                </div>
+                <hr className={style.main__line}></hr>
+            </div> 
+            
+            <div className={style.rightBlock}></div>
         </main>
-        <div className={style.blackline}>
+        <div className={style.blackline}></div>
+        {/* <div className={style.blackline}>
         <span className={style.line__text}>твое творческое состязание <img src={ellipse} alt='ellips'/> твое творческое состязание <img src={ellipse} alt='ellips'/> твое творческое состязание <img src={ellipse} alt='ellips'/> твое творческое состязание <img src={ellipse} alt='ellips'/> твое творческое состязание <img src={ellipse} alt='ellips'/></span>
-        </div>
+        </div> */}
 
     </div>
 }
