aa38df145af2e24c5c6a23f658b19dcf50e005f5
sonia13marker <soniamarker13@gmail.com>
2023-01-23 22:00:16 +0300

    адаптация
    адаптация под мобильные и более версии для главной страницы

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,18 +4,30 @@ import { HeaderMenuFull } from '../../components/HeaderMenuFull/HeaderMenuFull';
 import { MenuHamb } from '../../components/MenuHamb/MenuHamb';
 import MediaQuery from 'react-responsive';
 import { MainContent } from '../../components/MainContent/MainContent';
+import useMediaQuery from 'react-responsive';
 
-
+const Mobile = ({children}) => {
+    const isMobile = useMediaQuery({minWidth: 699, maxWidth: 700})
+    return isMobile ? children : null
+}
 
 export function MainPage () {
+    
+  
     return <div>
         <MediaQuery minWidth={700}>
             <HeaderMenuFull />
         </MediaQuery>
-        <MediaQuery maxWidth={320}>
+        <MediaQuery maxWidth={699}> 
             <MenuHamb/>
         </MediaQuery>
-        
+
+{/*проба - не работает, тут пока просто ни-че-го*/}
+        <Mobile>
+            <MenuHamb/>
+        </Mobile>
+
+
         {/*подключение контента под меню (шапкой) */}
         <MainContent />
 
