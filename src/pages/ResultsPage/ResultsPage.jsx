a0a6664857248baaa27293a54090ff323a486abc
sonia13marker <soniamarker13@gmail.com>
2023-02-15 16:10:50 +0300

    стили для основной страницы

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -7,16 +7,25 @@ import { OneProjectResult } from '../../components/OneProjectResult/OneProjectRe
 import winners from '../../constants/mock1.json';
 
 export function ResultsPage () {
-    console.log(winners);
-    return (<div>
+    return (<div className={style.yoi}>
 
             <HeaderMenuFull />
 
         <main>
+            {/*полоска с заголовоком */}
+            <div className={style.title_line}>
+                <h2 className={style.title__h2}>Результаты</h2>
+                <div className={style.line__wrap}>
+                    {/*значения должны подставляться*/}
+                    <h2 className={style.title__h2}>Django</h2>
+                    <h2 className={style.title__h2}>#2023</h2>
+                </div>
+            </div>
+
+
             {
             Object.values(winners).map((winner, id) => {
             return (Object.values(winner).map((win, di)=>{
-                console.log(win);
                 return (<section key={id} className={style.one_project}>
                     <OneProjectResult win={win}/>
                 </section>)
