f3a3ac87f32a5ae65a93261fbfb3341470967e1e
sonia13marker <soniamarker13@gmail.com>
2023-02-27 21:33:58 +0300

    смена структуры страницы "конкурсы"
    добавление года, попытка в смену аккордеона

--- src/components/YearContests/YearContests.jsx
+++ src/components/YearContests/YearContests.jsx
@@ -3,20 +3,56 @@ import { OneYear } from '../OneYear/OneYear';
 
 /*тут отображение всех конкурсов за один год */
 export function YearContests ({inf}) {
+    
+    let c = Object.keys(inf).sort((a,b) => a.key - b.key)
+    console.log(c)
+        // Object.keys(inf).map((dtf) => {
+        // dtf.sort((a,b) => a.price - b.price))
+        // console.log(dtf)
+        // }
+        
+
+        // Object.keys(inf)
+        // .map((dtf) => {return <span>{dtf}</span>})
+        
+
     return (<>
+    {/* <li>{c}</li> */}
+    <div className={style.years}>
+{
+
+
+
+
+    
+    //  const recentprods = products
+    //  .sort((a,b) => a.price - b.price)
+    //      // if you want to change the sorting direction: b.price - a.price
+    //  .map(prod => {
+    //     return <TableRow name={prod.name} price={prod.price} />
+    //  });
+
+}
+</div>
+
+{/* </div>    */}
 
 
             {/*контейнер с годом и стрелкой списка */}
             <section>
                 <span>
-
-                    <h2 className={style.year__h2}>
+                <div >
+                    {
+                Object.keys(inf).map((dtf) => {return <h2 className={style.year__h2}>{dtf}</h2>})
+                    }
+                    </div>
+                    <div>
                     {Object.values(inf).map((innf, ids) => {
-                        console.log(innf)
+                        
           return (<OneYear key={ids} {...innf}/>);
         })}
        
-                    </h2>
+                    </div>
                     
                 </span>
                 
