48718b2f280ea04dece285d0eca83a469964c75a
sonia13marker <soniamarker13@gmail.com>
2023-03-28 23:01:24 +0300

    исправления страницы "конкурсы"
    изменение структуры массива, удаление ненужной страницы, добавление стилей к годам

--- src/components/OneYear/OneYear.jsx
+++ src/components/OneYear/OneYear.jsx
@@ -1,38 +1,31 @@
-import { OneYearContest } from '../OneYearContest/OneYearContest';
+import {OneYearContest} from '../OneYearContest/OneYearContest';
 import style from './style.module.css'
 import arrow_top from '../../images/arrow__list_top.svg';
 import arrow_bottom from '../../images/arrow__list_bottom.svg';
-import { React, useState} from 'react';
-import classnames from "classnames";
-import { GoBtn } from '../GoBtn/GoBtn';
+import {React, useState} from 'react';
 
 
-export function OneYear(innf){
-    console.log(innf);
-    
+export function OneYear({year, items}) {
     const [isActive, setIsActive] = useState(false);
-    
-    return <div className={style.preekol}>
 
-{/*аккордеон */}
-  <div className={style.accordeon__title} onClick={() => setIsActive(!isActive)}>
+    return (
+        <div className={style.preekol}>
+            {/*аккордеон */}
+            <div className={style.accordeon__title} onClick={() => setIsActive(!isActive)}>
 
-{/*год и стрелка для раскрытия */}
-<span className={style.arrow_wrap}>
-  <p>#2023</p>
-  <span className={style.year__arrow_top}>
-      {isActive ? <img src={arrow_top} alt=""/> : <img src={arrow_bottom}/>}
-  </span>  
-</span>
-</div>
-  { isActive && <div className={style.accordeon__content}>
-    {Object.values(innf).map((inf, id) => {
-    console.log(inf)
-    return <OneYearContest key={id} {...inf}/>;
-    })}
-  </div>}
-        
+                {/*год и стрелка для раскрытия */}
+                <span className={style.arrow_wrap}>
+                  <p className={style.year__p}>#{year}</p>
 
-        
-</div>
+                  <span className={style.year__arrow_top}>
+                      {isActive ? <img src={arrow_top} alt=""/> : <img src={arrow_bottom}/>}
+                  </span>
+                </span>
+            </div>
+
+            {isActive && <div className={style.accordeon__content}>
+                {items.map((inf, id) => <OneYearContest key={id} {...inf}/>)}
+            </div>}
+        </div>
+    )
 }
--- src/pages/ContestPage/ContestPage.jsx
+++ src/pages/ContestPage/ContestPage.jsx
@@ -1,41 +1,41 @@
 import style from './style.module.css';
 import MediaQuery from 'react-responsive';
-import { HeaderMenuFull } from '../../components/HeaderMenuFull/HeaderMenuFull';
-import { YearContests } from '../../components/YearContests/YearContests';
+import {HeaderMenuFull} from '../../components/HeaderMenuFull/HeaderMenuFull';
 import informations from '../../constants/mock.json';
-import { FooterSmall } from '../../components/FooterSmall/FooterSmall';
-import { Footer } from '../../components/Footer/Footer';
+import {FooterSmall} from '../../components/FooterSmall/FooterSmall';
+import {Footer} from '../../components/Footer/Footer';
+import {OneYear} from "../../components/OneYear/OneYear";
+
 /*тут будет инфа о всех конкурсах (то есть инфа о всех годах)*/
 
-export function ContestPage () {
-    return (<div className={style.wr}>
+export function ContestPage() {
+    return (
+        <div className={style.wr}>
+            <HeaderMenuFull/>
 
-            <HeaderMenuFull />
+            {/*тут будет инфа о всех конкурсах*/}
+            <div className={style.container}>
+                <h2 className={style.container__h2}>Конкурсы</h2>
 
-         {/*тут будет инфа о всех конкурсах*/}
-         <div className={style.container}>
-            <h2 className={style.container__h2}>Конкурсы</h2>
-            <div className={style.prikol}>
-            {
-            Object.values(informations).map((info ,id)=>{      
-                return (Object.values(info).map((inf, di)=>{
-                    console.log(inf);
-                    
-                    return (<YearContests key={di} inf={inf}></YearContests>)
-                    
-                })
-            )})
-        }
+                <div className={style.prikol}>
+                    {/* {
+                        Object.entries(informations).sort((first, second) => second[0] - first[0])
+                    } */}
+                    {Object.entries(informations).sort((first, second) => second[0] - first[0]).map(([key, value]) => (
+                        <OneYear
+                            key={key}
+                            year={key}
+                            items={value}/>
+                    ))}
+                </div>
+            </div>
+
+            <MediaQuery minWidth={576}>
+                <Footer/>
+            </MediaQuery>
+            <MediaQuery maxWidth={575}>
+                <FooterSmall/>
+            </MediaQuery>
         </div>
-        </div>
-        
-
-        <MediaQuery minWidth={576}>
-            <Footer/>
-        </MediaQuery>
-        <MediaQuery maxWidth={575}> 
-            <FooterSmall />
-        </MediaQuery>
-
-    </div>)
+    );
 }
